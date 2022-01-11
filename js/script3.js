b719c63b31397803c03d0188e73545e0b9a45759
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2022-01-11 18:58:04 +0300

    fourth exercise

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+let souvenirWeight = 75;
+let trifleWeight = 112;
+
+let souvenirNum;
+souvenirNum = prompt("Введите количество сувенира:");
+
+let trifleNum;
+trifleNum = prompt("Введите количество безделушки:");
+
+let resoult = souvenirWeight * souvenirNum + trifleWeight * trifleNum;
+alert("Общий вес груза составляет:" + resoult);
