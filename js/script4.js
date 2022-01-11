e773972947e58096d13740355fce7e9dd5ab88f9
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2022-01-11 20:13:14 +0300

    fifth exercise

---
+++ js/script4.js
@@ -0,0 +1,15 @@
+/*let a;
+a = prompt("введите первое число:");
+
+let b; 
+b = prompt("введите второе число:");
+
+
+let result = [a + b, a - b, a * b, a / b, a % b, a ** b];
+alert(result);*/
+
+let a = 6;
+let b = 2;
+
+let result = [a + b, a - b, a * b, a / b, a % b, a ** b];
+console.log(result);
